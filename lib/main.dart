de19e5d46448a82ac838c1de1d139f42e2af6d7e
irahulcse <1rahulchandra1@gmail.com>
2020-04-07 17:30:05 +0530

    Automatic Commit 2020-04-07-17:30:05

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Calculator App',
-      theme: new ThemeData(primarySwatch: Colors.red),
+      theme: ThemeData.dark(),
       home: new HomePage(),
     );
   }
